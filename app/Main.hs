5a0ddb46fdfa367d545533e134463b3de0578374
mkrooted <koreshov.m@gmail.com>
2020-04-01 20:32:31 +0300

    Deletion. Min/Max element. Also i've changed function naming style and arguments order (now `insert` can be used as first arg to fold)

--- app/Main.hs
+++ app/Main.hs
@@ -3,18 +3,18 @@ module Main where
 import Text.Read
 import L5
 
-elementinput :: L5.Tree Int -> IO (L5.Tree Int)
-elementinput tree = do
+elementInput :: L5.Tree Int -> IO (L5.Tree Int)
+elementInput tree = do
   putStrLn "Add element: "
   line <- getLine
   case readMaybe line :: Maybe Int of
-    Just elem -> elementinput $ L5.insert elem tree
+    Just elem -> elementInput $ L5.insert tree elem
     _ -> do
       putStrLn "End of input"
       return tree
 
 main :: IO ()
 main = do
-  tree <- elementinput L5.EmptyTree
+  tree <- elementInput L5.EmptyTree
   print tree
 
--- src/L5.hs
+++ src/L5.hs
@@ -1,25 +1,59 @@
 module L5
-    ( Tree(EmptyTree),
-      singleton,
-      insert,
-      intree
-    ) where
+  ( Tree(EmptyTree)
+  , singleton
+  , insert
+  , inTree
+  , delete
+  , treeMin
+  , treeMax
+  , popTreeMin
+  ) where
 
+data Tree t
+  = EmptyTree
+  | Node t (Tree t) (Tree t)
+  deriving (Show)
 
-data Tree t = EmptyTree | Node t (Tree t) (Tree t) deriving (Show)
+singleton :: t -> Tree t
+singleton x = Node x EmptyTree EmptyTree
 
-singleton :: t -> Tree t  
-singleton x = Node x EmptyTree EmptyTree  
+insert :: (Ord t) => Tree t -> t -> Tree t
+insert EmptyTree x = singleton x
+insert (Node val left right) x
+  | x >= val = Node val left (insert right x)
+  | x < val = Node val (insert left x) right
 
-insert :: (Ord t) => t -> Tree t -> Tree t
-insert x EmptyTree = singleton x
-insert x (Node val left right)
-  | x >= val = Node val left (insert x right) 
-  | x < val  = Node val (insert x left) right
+treeMin :: (Ord t) => Tree t -> t
+treeMin (Node val EmptyTree _) = val
+treeMin (Node _ left _)        = treeMin left
 
-intree :: (Ord t) => t -> Tree t -> Bool
-intree x EmptyTree = False
-intree x (Node val left right)
+treeMax :: (Ord t) => Tree t -> t
+treeMax (Node val _ EmptyTree) = val
+treeMax (Node _ _ right)       = treeMax right
+
+popTreeMin :: (Ord t) => Tree t -> (t, Tree t)
+popTreeMin (Node val EmptyTree right) = (val, right)
+popTreeMin (Node val left right) = (minVal, Node val newLeft right)
+  where
+    (minVal, newLeft) = popTreeMin left
+
+delete :: (Ord t) => Tree t -> t -> Tree t
+delete EmptyTree x = error "Element not found"
+delete (Node v EmptyTree left) x 
+  | v == x = left
+delete (Node v right EmptyTree) x 
+  | v == x = right
+delete (Node val left right) x
+  | x > val = Node val left (delete right x)
+  | x < val = Node val (delete left x) right
+  | x == val = Node newVal left newRight
+  where
+    (newVal, newRight) = popTreeMin right
+
+inTree :: (Ord t) => Tree t -> t -> Bool
+inTree EmptyTree x = False
+inTree (Node val left right) x
   | x == val = True
-  | x > val  = intree x right
-  | x < val  = intree x left
+  | x > val = inTree right x
+  | x < val = inTree left x
+
